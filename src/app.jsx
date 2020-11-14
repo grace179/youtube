7583890c852014a283fd718ba5f3eb3405ee190f
carrie9497 <67547279+carrie9497@users.noreply.github.com>
2020-11-14 13:03:09 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyDQ9i0AzvgItTHHGlVk4Us-Do_4q_XL1xI", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  
   return (
-    <h1>hello</h1>
+    <VideoList key={videos.id} videos={videos}/>
   );
 }
 
