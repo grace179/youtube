d91cff000860ba3924001326975e5a9de5265cd4
carrie9497 <67547279+carrie9497@users.noreply.github.com>
2020-11-14 21:40:32 +0900

    add search

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import './app.css';
+import React, { useEffect, useState } from 'react';
+import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
-function App() {
-
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-
+  const search = query => {
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
+  };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyDQ9i0AzvgItTHHGlVk4Us-Do_4q_XL1xI", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  }, []);
-  
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
+  },[youtube]);
   return (
-    <VideoList key={videos.id} videos={videos}/>
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
   );
 }
 
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -4,11 +4,11 @@ import styles from './video_item.module.css';
 const VideoItem = ({video: {snippet}}) => (
     <li className={styles.container}>
         <div className={styles.video}>
-        <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="thumbnail image"/>
-        <div className={styles.metadata}>
-            <p className={styles.title}>{snippet.title}</p>
-            <p className={styles.channel}>{snippet.channelTitle}</p>
-        </div>
+            <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="thumbnail"/>
+            <div className={styles.metadata}>
+                <p className={styles.title}>{snippet.title}</p>
+                <p className={styles.channel}>{snippet.channelTitle}</p>
+            </div>
         </div>
     </li>
     );
