3a2ef758fd3f31ebe2977a5bae379c3d14ea5ead
carrie9497 <67547279+carrie9497@users.noreply.github.com>
2020-11-15 16:53:58 +0900

    selected video detail
    selected video detail

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  }
   const search = query => {
+    setSelectedVideo(null);
     youtube
       .search(query) //
       .then(videos => setVideos(videos));
@@ -15,11 +22,21 @@ function App({ youtube }) {
     youtube
       .mostPopular() //
       .then(videos => setVideos(videos));
-  },[youtube]);
+  },[]);
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && 
+          (<div className={styles.detail}>
+              <VideoDetail video={selectedVideo}/>
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo} 
+          display={ selectVideo ? 'list':'grid'}/>
+        </div>
+      </section>
     </div>
   );
 }
