6d11b12218dd61e77ded504d18bbd71dded1ac65
carrie9497 <67547279+carrie9497@users.noreply.github.com>
2020-11-15 18:30:23 +0900

    update video

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -6,9 +6,10 @@ const VideoDetail = ({video}) => (
     <section className={styles.detail}>
         <iframe 
         className={styles.video}
+        title="youtube player"
         id="ytplayer" type="text/html" width="100%" height="500px"
         src={`https://www.youtube.com/embed/${video.id}`}
-        frameborder="0" allowfullscreen></iframe>
+        frameBorder="0" allowFullscreen></iframe>
         <h2>{video.snippet.title}</h2>
         <h3>{video.snippet.channelTitle}</h3>
         <pre className={styles.description}>{video.snippet.description}</pre>
